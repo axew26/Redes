c88ada9ca4a965b996acd58b04a8976e7d9b7958
axew26 <axew26@github.com>
2018-01-28 17:59:56 -0500

    Creacion Main
    Se creo la interface principal con botones

--- Redes/src/redes/Redes.java
+++ Redes/src/redes/Redes.java
@@ -16,6 +16,8 @@ public class Redes {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        IntefaceInicio form = new IntefaceInicio();
+        form.setVisible(true);
     }
     
 }
